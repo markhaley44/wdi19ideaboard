c05f8d7bbee4547049ef369e280638101ae6ef13
king0120 <king0120@gmail.com>
2018-11-08 15:20:44 -0500

    added components

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require('express')
 const app = express()
 const routes = require('./routes/index')
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use(express.static(__dirname + '/client/build/'));
+app.use(express.static(__dirname + '/client/build/'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
+  res.sendFile(__dirname + '/client/build/index.html')
 })
 
 app.use('/', routes)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server is listening on PORT: ${PORT}`)
+  console.log(`Server is listening on PORT: ${PORT}`)
 })
-
