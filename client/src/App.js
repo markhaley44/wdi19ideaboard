8cd79a78178e7ca8d5d681c6aa52113673591515
Mark Haley <markhaley44@gmail.com>
2019-01-25 15:17:57 -0500

    pushing merges

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
+<<<<<<< Updated upstream
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import UserList from './components/Users/UserList';
 import SingleUser from './components/Users/SingleUser';
+=======
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+>>>>>>> Stashed changes
 
 class App extends Component {
   render() {
     return (
+<<<<<<< Updated upstream
       <div>
         <Router>
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route exact path="/users" component={UserList}/>
             <Route exact path="/users/:userId" component={SingleUser}/>
+=======
+      <div >
+        <Router>
+          <Switch>
+            <Route exact path="/" Component={LandingPage} />
+>>>>>>> Stashed changes
           </Switch>
         </Router>
       </div>
