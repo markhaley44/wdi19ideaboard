b5f83e7050ec9d81cfebfcea7fbdea310bf843db
Mark Haley <markhaley44@gmail.com>
2019-01-27 20:13:56 -0500

    modified client

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,17 @@
-<<<<<<< Updated upstream
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import UserList from './components/Users/UserList';
 import SingleUser from './components/Users/SingleUser';
-=======
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
->>>>>>> Stashed changes
-
 class App extends Component {
   render() {
     return (
-<<<<<<< Updated upstream
       <div>
         <Router>
           <Switch>
             <Route exact path="/" component={LandingPage} />
-            <Route exact path="/users" component={UserList}/>
-            <Route exact path="/users/:userId" component={SingleUser}/>
-=======
-      <div >
-        <Router>
-          <Switch>
-            <Route exact path="/" Component={LandingPage} />
->>>>>>> Stashed changes
+            <Route exact path="/users" component={UserList} />
+            <Route exact path="/users/:userId" component={SingleUser} />
           </Switch>
         </Router>
       </div>
--- client/src/components/Users/UserList.js
+++ client/src/components/Users/UserList.js
@@ -1,31 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import axios from axios
-
-class UserList extends Component {
-    state = {
-        users: [{}]
-    }
-    componentDidMount() {
-        this.getAllUsers()
-    }
-    getAllUsers = () => {
-        axios.get(`api/users`)
-            .then((res) => this.setState({ users: res.data }))
-    }
-    render() {
-        return (
-            <div>
-                <h1>Hey from the user view</h1>
-                {this.state.users.map((user, i) =>
-                    <div key={i}>
-                        <h3>{user.username}</h3>
-                    </div>)}
-            </div>
-        )
-    }
-}
-=======
 import axios from 'axios'
 import AddUserForm from './AddUserForm';
 import styled from 'styled-components'
@@ -74,4 +47,3 @@ class UserList extends Component {
 }
 
 export default UserList;
->>>>>>> ece385e52a8016e504ef7ad95628186c73115863
